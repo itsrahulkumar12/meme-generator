655140c89452272167b3d2e47fd4ec1e63691587
Rahul Kumar <iamrahulkumar.dev@gmail.com>
2023-01-20 00:08:34 +0530

    added meme Text

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -24,17 +24,45 @@ const Meme = () => {
     });
   };
 
+  function handleChange(event) {
+    const {name, value} = event.target  
+    setMeme(prevMeme => {
+        return {
+          ...prevMeme,
+          [name] : value
+        }
+      })
+  }
+
   return (
     <main>
       <div className="form">
-        <input type="text" placeholder="Top text" className="form-input" />
-        <input type="text" placeholder="Bottom text" className="form-input" />
+        <input 
+          type="text" 
+          placeholder="Top text" 
+          className="form-input" 
+          value={meme.topText}
+          name="topText"
+          onChange={handleChange}
+        />
+        <input 
+          type="text" 
+          placeholder="Bottom text" 
+          className="form-input" 
+          value={meme.bottomText}
+          name="bottomText"
+          onChange={handleChange}
+        />
+
         <button onClick={getMemeImg} className="form-btn">
           Get a new meme image
         </button>
+
       </div>
       <div className="meme-img-container">
         <img className="meme-image" src={meme.randomImage} alt="meme Image" />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
       </div>
     </main>
   );
